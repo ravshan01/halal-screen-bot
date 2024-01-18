7923e0b1dfa09fc3be38074c23bada87368582f1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-18 22:01:25 +0500

    create core/config

---
+++ core/config.py
@@ -0,0 +1,13 @@
+from pathlib import PurePath
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from settings import ROOT_DIR
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=str(PurePath(ROOT_DIR, ".env")), env_file_encoding="utf-8"
+    )
+    bot_token: str
+
+
+config = Settings()
