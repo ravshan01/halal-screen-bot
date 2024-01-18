3e260d5f759e35a4ba13c45744b8bd5328524513
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-18 22:03:03 +0500

    add max_video_size env variable

--- core/config.py
+++ core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
         env_file=str(PurePath(ROOT_DIR, ".env")), env_file_encoding="utf-8"
     )
     bot_token: str
+    max_video_size: int
 
 
 config = Settings()
