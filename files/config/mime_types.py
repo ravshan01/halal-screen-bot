69d1cfbb579c9c5e9f6bbbf6b60d4af1c5c93eb4
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-18 22:07:24 +0500

    create files/config/mime_types

---
+++ files/config/mime_types.py
@@ -0,0 +1,18 @@
+IMAGE_MIME_TYPES = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/gif",
+    "image/tiff",
+    "image/bmp",
+]
+
+VIDEO_MIME_TYPES = [
+    "video/mp4",
+    "video/mpeg",
+    "video/ogg",
+    "video/quicktime",
+    "video/webm",
+    "video/x-matroska",
+    "video/x-msvideo",
+]
